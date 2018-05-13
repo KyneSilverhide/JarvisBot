bb832927abd000c4401640b78a4b599ef41dedfd
KyneSilverhide <kyne.silverhide@gmail.com>
2018-05-13 17:43:43 +0200

    rate setting

--- commands/nidecerf.js
+++ commands/nidecerf.js
@@ -5,13 +5,33 @@ const randomAnswers = [
   'Merci',
   "Il y aura bientôt un nouvel organigramme, je ne sais pas ce qu'on me réserve...",
   "Tu as l'âme d'un B1 Rebel",
+  "Dans mon coeur tu as toujours été plus haut dans l'organigramme",
 ];
 
+let rate = 10;
+
+const processAdminCommand = (client, message) => {
+  const commands = message.content.split(' ');
+  if (commands.length > 1) {
+    if (commands.length === 3 && commands[1].toLowerCase === 'rate') {
+      rate = Number(commands[2]);
+      message.reply(`Je donnerai mon avis sur ${rate}% des messages`);
+    }
+  }
+};
+
 exports.process = (client, message) => {
-  const random = Math.floor(Math.random() * 100);
-  console.log(random);
-  if (random < 5) {
-    const index = Math.floor(Math.random() * randomAnswers.length);
-    message.reply(randomAnswers[index]);
+  if (message.content.toLowerCase().indexOf('ta gueule nico') > -1) {
+    message.reply("Ok, je parlerai moins... de toute façon plus personne ne m'écoute");
+    rate /= 2;
+    console.log(`Rate is now ${rate}`);
+  } else if (message.content.toLowerCase().startsWith('/nidecerf')) {
+    processAdminCommand(client, message);
+  } else {
+    const random = Math.floor(Math.random() * 100);
+    if (random < rate) {
+      const index = Math.floor(Math.random() * randomAnswers.length);
+      message.reply(randomAnswers[index]);
+    }
   }
 };
